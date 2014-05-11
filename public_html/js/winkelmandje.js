6d00824b0f9067ea2b33ccd9b83813fe20e08dbb
schutyser <thijs.schutyser@gmail.com>
2014-05-11 12:54:16 +0200

    placingorder

--- public_html/js/winkelmandje.js
+++ public_html/js/winkelmandje.js
@@ -104,6 +104,7 @@ function updateWinkelmand(id, prijs, aantal) {
 function addTheme() {
     var arrayTheme = [];
     var theme = $('input[name=Thema]:checked', '#themaSelect').val();
+    window.alert(theme);
     arrayTheme.push(theme);
     setWinkelmandArray(arrayTheme);
     emailLevering();
@@ -113,6 +114,7 @@ function addTheme() {
 //Personalisatie van de voucher: boodschap kiezen + gegevens ontvanger voucher
 function addBoodschap() {
     var arrayBoodschap = getWinkelmandArray();
+    window.alert(arrayBoodschap);
     var fout = "Sommige verplichte velden zijn niet ingevuld: ";
 
     var boodschapInhoud = $('#boodschapInhoud').val();
